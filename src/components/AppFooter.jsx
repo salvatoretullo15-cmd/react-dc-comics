605086bf57710ca2c609422cc36a58c6be5fd9ab
salvatore tullo <salvatoretullo15@gmail.com>
2026-03-06 18:30:07 +0100

    completamento main e bluebar

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -3,7 +3,7 @@ export default function AppFooter(){
 
   return (
     <footer> 
-      Footer
+      
     </footer>
   )
 }
--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -3,7 +3,60 @@ export default function AppMain(){
 
   return (
     <main> 
-      Main
+      <div className="container">
+        <p> 
+          <h1>DC Comics: Il Rinnovamento del 2025-2026</h1>
+          La DC Comics, fondata nel 1934, è una delle più importanti 
+          case editrici di fumetti al mondo e sta vivendo una fase di 
+          profondo rinnovamento sia editoriale che cinematografico tra 
+          il 2025 e il 2026.<br/>
+          <h2>Cinema e TV: Il nuovo DC Universe (DCU)</h2>
+          Sotto la guida di James Gunn e Peter Safran, il franchise 
+          cinematografico è stato resettato per dare vita al "Capitolo Uno: Dei e Mostri". <br/>
+          I progetti più attesi includono:<br/>
+          Superman: Il nuovo film dedicato all'Uomo d'Acciaio, diretto 
+          da James Gunn, è l'evento principale del 2025. L'uscita nelle 
+          sale italiane è fissata per il 9 luglio 2025.<br/>
+          Supergirl: Woman of Tomorrow: Film previsto per il 26 giugno 2026, 
+          con Craig Gillespie alla regia.<br/>
+          Clayface: Un nuovo progetto cinematografico annunciato per ottobre 2026.<br/>
+          Serie TV: Tra i titoli più attesi su Max figurano Peacemaker (Stagione 2) nel 2025 
+          e Lanterns, la serie dedicata alle Lanterne Verdi, prevista per il 2026.
+          The Batman - Parte II: Sebbene esterno alla continuity principale del DCU 
+          (etichetta Elseworlds), il sequel con Robert Pattinson è programmato 
+          per ottobre 2026.<br/> 
+          <h2>Fumetti: Grandi Eventi e l'Absolute Universe</h2>
+          Nel 2025, la divisione editoriale ha lanciato iniziative ambiziose 
+          per attirare nuovi lettori. <br/>
+          Absolute Universe: Una nuova linea editoriale che reimmagina le icone classiche 
+          (Superman, Batman, Wonder Woman) in un contesto moderno e privo del peso della 
+          continuity passata.<br/>
+          Crossover DC/Marvel: In una storica collaborazione, nel 2025 sono stati pubblicati
+          albi speciali come Batman/Deadpool e Deadpool/Batman, segnando il primo grande 
+          incontro tra i due universi dopo decenni.<br/>
+          DC K.O.: Un evento cosmico strutturato come un torneo che coinvolge i principali eroi dell'universo DC, presentato durante il Comic-Con 2025. 
+          <h2>I Pilastri della DC</h2>
+          L'universo DC continua a ruotare attorno alla Justice League, 
+          i cui membri principali sono: 
+          Superman: Considerato l'eroe più forte e simbolo di speranza.<br/>
+          Batman: Il detective di Gotham City, protagonista di saghe 
+          come Hush 2 prevista per il 2025.<br/>
+          Wonder Woman: Icona di forza e giustizia.<br/>
+          Altri membri: Flash, Lanterna Verde, Aquaman, Cyborg e Martian Manhunter. 
+        </p>
+      </div>
+        <div className="blu-bar">
+          <img src="/public/buy-comics-digital-comics.png" alt="" />
+          <button>DIGITAL COMICS</button>
+          <img src="/public/buy-comics-merchandise.png" alt="" />
+          <button>DC MERCHANDISE</button>
+          <img src="/public/buy-comics-subscriptions.png" alt="" />
+          <button>SUBSCRIPTION</button>
+          <img src="/public/buy-comics-shop-locator.png" alt="" />
+          <button>COMIC SHOP LOCATOR</button>
+          <img src="/public/buy-dc-power-visa.svg" alt="" />
+          <button>DC POWER VISA</button>
+        </div>
     </main>
   )
 }
