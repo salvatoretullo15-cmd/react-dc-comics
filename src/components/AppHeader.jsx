5382403349c51d7fcf708130761c95ba97b39a15
salvatore tullo <salvatoretullo15@gmail.com>
2026-03-06 17:07:03 +0100

    prima parte header pseudo completa

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -2,8 +2,20 @@ export default function AppHeader(){
 
 
   return (
-    <header> 
-      Header
+    <header>
+      <nav>
+          <img src="/public/dc-logo.png" alt="DC Logo" /> 
+          <a href="#">CHARACTERS</a>
+          <a href="#" className="active">COMICS</a>
+          <a href="#">MOVIES</a>
+          <a href="#">TV</a>
+          <a href="#">GAMES</a>
+          <a href="#">COLLECTIBLES</a>
+          <a href="#">VIDEOS</a>
+          <a href="#">FANS</a>
+          <a href="#">NEWS</a>
+          <a href="#">SHOP</a>
+      </nav>
     </header>
   )
 }
